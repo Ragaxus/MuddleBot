82eac1256553849460ff57e7a1062300f7974be7
Ragaxus <ragaxus@gmail.com>
2020-07-22 21:46:09 -0500

    Add randColor command

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 const Discord = require('discord.js');
 var Scryfall = require('scryfall-client');
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
@@ -31,6 +44,13 @@ bot.on('message', msg => {
         console.warn('Something went wrong!');
         console.warn(err);
     }
+  }
+  else if (msg.content.search("!randcolor")>=0) {
+    match = msg.content.match(/!randcolor (\d)/);
+    if (match.len > 1) numColors = match[1];
+    else numColors = 1;
+    colors = shuffle(["white","blue","black","red","green"]).slice(0,numColors+1);
+    msg.reply(colors.join(", "));
   }
 
 });
